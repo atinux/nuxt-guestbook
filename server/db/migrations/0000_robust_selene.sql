713dcb0cc15721f08f616885b0f1efeea5764296
Sébastien Chopin <seb@nuxt.com>
2023-11-09 19:04:39 +0100

    chore: update schema

--- server/db/migrations/0000_robust_selene.sql
+++ server/db/migrations/0000_robust_selene.sql
@@ -1,8 +1,7 @@
 CREATE TABLE `quotes` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`author` text NOT NULL,
-	`body` text NOT NULL,
-	`created_at` integer NOT NULL
+	`body` text NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `quotes_author_unique` ON `quotes` (`author`);
--- server/db/schema.ts
+++ server/db/schema.ts
@@ -3,8 +3,7 @@ import { sqliteTable, text, integer, unique } from 'drizzle-orm/sqlite-core'
 export const quotes = sqliteTable('quotes', {
   id: integer('id').primaryKey(),
   author: text('author').notNull(),
-  body: text('body').notNull(),
-  date: integer('created_at', { mode: 'timestamp' }).notNull(),
+  body: text('body').notNull()
 }, (t) => ({
   authorIdx: unique().on(t.author)
 }))
