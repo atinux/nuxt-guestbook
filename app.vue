1361116504feeeb13bf5f94bb2fbcbe5c46bfbfb
Sébastien Chopin <seb@nuxt.com>
2023-11-10 17:29:44 +0100

    let's gooo

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <script setup>
+const { loggedIn, clear } = useUserSession()
+
 useSeoMeta({
   title: 'Nuxt Guestbook',
   description:
@@ -7,5 +9,21 @@ useSeoMeta({
 </script>
 
 <template>
-  <NuxtPage />
+  <UHeader>
+    <template #logo>
+      Nuxt Guestbook
+    </template>
+    <template #right>
+      <UColorModeButton />
+      <UButton v-if="loggedIn" color="gray" @click="clear">
+        Logout
+      </UButton>
+      <UButton v-else icon="i-simple-icons-github" color="gray" to="/auth/github" external>
+        Login with GitHub
+      </UButton>
+    </template>
+  </UHeader>
+  <UContainer>
+    <NuxtPage />
+  </UContainer>
 </template>
