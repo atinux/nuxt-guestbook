37716276d03139be26d75bad1ff495da47d76e35
Sébastien Chopin <seb@nuxt.com>
2023-11-10 16:22:29 +0100

    chore: demo

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <script setup>
+const { loggedIn, clear } = useUserSession()
+
 useSeoMeta({
   title: 'Nuxt Guestbook',
   description:
@@ -7,5 +9,21 @@ useSeoMeta({
 </script>
 
 <template>
-  <NuxtPage />
+  <UHeader>
+    <template #logo>
+      Nuxt Guestbook
+    </template>
+    <template #right>
+      <UColorModeButton />
+      <UButton v-if="loggedIn" color="gray" @click="clear">
+        Logout
+      </UButton>
+      <UButton v-else to="/auth/github" external color="gray" icon="i-simple-icons-github">
+        Login with GitHub
+      </UButton>
+    </template>
+  </UHeader>
+  <UContainer>
+    <NuxtPage />
+  </UContainer>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,49 @@
-<script setup>
+<script setup lang="ts">
+const { loggedIn, user } = useUserSession()
+
+const { data: comments } = await useFetch('/api/comments')
+const otherComments = computed(() => comments.value?.filter((c) => c.author !== user.value?.username))
+const userComment = computed(() => comments.value?.find((c) => c.author === user.value?.username))
+const userCommentBody = ref(userComment.value?.body ?? '')
+const editing = ref(userCommentBody.value === '')
+async function saveComment() {
+  if (userCommentBody.value.trim() === '') {
+    return
+  }
+  editing.value = false
+  await $fetch('/api/comment', {
+    method: 'PUT',
+    body: {
+      body: userCommentBody.value
+    }
+  })
+}
 </script>
 
 <template>
-  <div>Hello Vue Toronto</div>
+  <UPageBody>
+    <UPageColumns>
+      <UPageCard v-if="!loggedIn" class="text-center">
+        <UButton to="/auth/github" external color="black" icon="i-simple-icons-github">
+          Login to comment
+        </UButton>
+      </UPageCard>
+      <UPageCard v-else @dbclick="editing = true">
+        <UButton v-if="!editing" icon="i-heroicons-pencil" color="gray" variant="ghost" class="absolute top-2 right-2" @click="editing = true" />
+        <UTextarea v-if="editing" v-model="userCommentBody" :rows="1" autofocus autoresize @blur="saveComment" />
+        <q v-else>{{ userCommentBody }}</q>
+        <div class="flex items-center gap-2 mt-4">
+          <UAvatar :src="`https://github.com/${user.username}.png`" :alt="user.username" size="md" />
+          <span class="font-semibold">{{ user.username }}</span>
+        </div>
+      </UPageCard>
+      <UPageCard v-for="comment of otherComments" :key="comment.id" :to="`https://github.com/${comment.author}`" target="_blank">
+        <q>{{ comment.body }}</q>
+        <div class="flex items-center gap-2 mt-4">
+          <UAvatar :src="`https://github.com/${comment.author}.png`" :alt="comment.author" size="md" />
+          <span class="font-semibold">{{ comment.author }}</span>
+        </div>
+      </UPageCard>
+    </UPageColumns>
+  </UPageBody>
 </template>
