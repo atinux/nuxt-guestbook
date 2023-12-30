3371db9fbfea6d2df785aa98fc08f59db1935bd0
Sébastien Chopin <seb@nuxt.com>
2023-12-30 13:53:19 +0100

    feat: leverage NuxtHub

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,24 +1,13 @@
-import { drizzle as drizzleD1, DrizzleD1Database } from 'drizzle-orm/d1'
-import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3'
-// @ts-ignore
-import Database from 'better-sqlite3'
-import { join } from 'pathe'
+import { drizzle, LibSQLDatabase } from 'drizzle-orm/libsql'
 
 export * as tables from '~/server/db/schema'
 
-let _db: DrizzleD1Database | BetterSQLite3Database | null = null
+let _db: LibSQLDatabase | null = null
 
 export const useDB = () => {
   if (!_db) {
-    if (process.env.DB) {
-      // d1 in production
-      _db = drizzleD1(process.env.DB)
-    } else if (process.dev) {
-      const sqlite = new Database(join(process.cwd(), './db.sqlite'))
-      _db = drizzle(sqlite)
-    } else {
-      throw new Error('No database configured for production')
-    }
+    // d1 in production
+    _db = drizzle(useDatabaseClient())
   }
   return _db
 }
