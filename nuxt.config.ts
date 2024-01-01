42083dacf4cc3d4ad34e6d2b97fba5246111592e
Akshara Hegde <akshara@blowhorn.com>
2024-01-01 12:26:59 +0530

    build - upgraded packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,8 @@
 export default defineNuxtConfig({
-  extends: ['@nuxt/ui-pro'],
-  modules: [
-    '@nuxt/ui',
-    'nuxt-auth-utils'
-  ],
-  ui: {
-    icons: ['heroicons', 'simple-icons']
-  },
   devtools: { enabled: true },
-})
+  extends: ["@nuxt/ui-pro"],
+  modules: ["@nuxt/ui", "nuxt-auth-utils"],
+  ui: {
+    icons: ["heroicons", "simple-icons"],
+  },
+});
